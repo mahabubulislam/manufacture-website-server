e5555705b343861f0ce76f3678234549b1b6a426
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-27 03:11:27 +0600

    parts load api added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { use } = require('express/lib/router');
+const ObjectId = require('mongodb').ObjectId
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,14 +14,19 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.orcrk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const partsCollection = client.db('manufacturer').collection('parts');
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        const partsCollection = client.db('manufacturer').collection('parts')
-       
+        // products load
+        app.get('/parts', async (req, res) => {
+            const parts = await partsCollection.find().toArray();
+            res.send(parts.reverse())
+        })
+      
     }
-    finally{
+    finally {
 
     }
 }
