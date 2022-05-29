5cc306063980a962e0c17e4e2ffed9ea9ab37888
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-29 13:20:23 +0600

    admin route protected

--- index.js
+++ index.js
@@ -83,6 +83,12 @@ async function run() {
             res.send(users);
         })
 
+        app.get('/admin/:email', async (req,res)=>{
+            const email = req.params.email;
+            const user = await usersCollection.findOne({email:email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin : isAdmin});
+        })
 
 
 
